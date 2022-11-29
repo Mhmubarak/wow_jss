1fb9241b521213a916b5fc5b9bd074fbad01414b
Mh Mubarak <110653722+Mhmubarak@users.noreply.github.com>
2022-11-29 22:44:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+new WOW().init();
+
+$(document).ready(function(){
+
+
+  });
+ // $( "#draggable" ).draggable();
+
+  //var availableTags = ["html","css","bootstrap","php"];
+    //$( "#tags" ).autocomplete({
+      //source: availableTags
+    //});
+
+//});
+
+
